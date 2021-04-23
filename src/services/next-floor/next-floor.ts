6840373bd8afbba70b2fd5d7e5e623d989d0c903
Ubu <ubald2b@gmail.com>
2021-04-23 13:26:49 +0200

    Use only one math.floor call instead of one for each recursion step

--- src/services/next-floor/next-floor.ts
+++ src/services/next-floor/next-floor.ts
@@ -1,6 +1,6 @@
 export function nextFloor(number: number): number {
   if (number >= 10) {
-    return nextFloor(Math.floor(number / 10)) * 10;
+    return nextFloor(number / 10) * 10;
   }
-  return number + 1;
+  return Math.floor(number) + 1;
 }
