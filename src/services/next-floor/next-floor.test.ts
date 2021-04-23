5d7d8ca18feb224b87e4a4d7534d3ffefd60ddc5
Ubu <ubald2b@gmail.com>
2021-04-23 13:06:51 +0200

    Fix next floor of 0 is 1

--- src/services/next-floor/next-floor.test.ts
+++ src/services/next-floor/next-floor.test.ts
@@ -4,7 +4,7 @@ import { nextFloor } from "./next-floor.ts";
 it("computes next level for 0", () => {
   const next = nextFloor(0);
 
-  expect(next).toBe(10);
+  expect(next).toBe(1);
 });
 
 it("computes next level for 10", () => {
