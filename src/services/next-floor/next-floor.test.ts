4970228850358bf1eb3d97aab10ab17effdf7f3a
Ubu <ubald2b@gmail.com>
2021-04-25 03:33:28 +0200

    Group tests inside describe for next floor function

--- src/services/next-floor/next-floor.test.ts
+++ src/services/next-floor/next-floor.test.ts
@@ -1,29 +1,35 @@
 import { nextFloor } from "./next-floor.ts";
-import {expect, it} from "../../deps.test.ts";
+import { describe, expect, it } from "../../deps.test.ts";
 
-const smallerThan10 = [...Array(10).keys()];
-smallerThan10.forEach((n) => {
-  it(`computes next level for ${n}`, () => {
-    const next = nextFloor(n);
+describe("Next floor function", () => {
+  describe("numbers smaller than 10", () => {
+    const smallerThan10 = [...Array(10).keys()];
+    smallerThan10.forEach((n) => {
+      it(`computes next level for ${n}`, () => {
+        const next = nextFloor(n);
 
-    expect(next).toBe(n + 1);
+        expect(next).toBe(n + 1);
+      });
+    });
   });
-});
 
-it("computes next level for 10", () => {
-  const next = nextFloor(10);
+  describe("numbers larger than 10", () => {
+    it("computes next level for 10", () => {
+      const next = nextFloor(10);
 
-  expect(next).toBe(20);
-});
+      expect(next).toBe(20);
+    });
 
-it("computes next level for 11", () => {
-  const next = nextFloor(11);
+    it("computes next level for 11", () => {
+      const next = nextFloor(11);
 
-  expect(next).toBe(20);
-});
+      expect(next).toBe(20);
+    });
 
-it("computes next level for 20", () => {
-  const next = nextFloor(20);
+    it("computes next level for 20", () => {
+      const next = nextFloor(20);
 
-  expect(next).toBe(30);
+      expect(next).toBe(30);
+    });
+  });
 });
